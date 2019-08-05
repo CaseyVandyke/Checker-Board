20c0907f3c774b70f804ac3520b84a2d6b647518
Cryosith <caseyvandyke@ymail.com>
2019-08-05 13:09:16 -0600

    checker pieces

---
+++ src/CheckerPiece.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CheckerPiece = () => {
+  return <div />;
+};
+
+export default CheckerPiece;
--- src/CheckerSquare.jsx
+++ src/CheckerSquare.jsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import CheckerPiece from "./CheckerPiece";
 
 const CheckerSquare = props => {
-  const squares = [];
+  const [squares, setSquare] = useState([]);
   const row = 8;
+  const firstPlayerPiece = 23;
+  const secondPlayerPiece = 40;
 
-  for (let i = 0; i < row; i++) {
-    for (let j = 0; j < row; j++) {
-      if ((i + j) % 2 === 0) {
-        squares.push(<div className="light square" />);
-      } else {
-        squares.push(<div className="dark square" />);
+  const checkerContainer = () => {
+    for (let i = 0; i < row; i++) {
+      for (let j = 0; j < row; j++) {
+        if ((i + j) % 2 === 0) {
+          squares.push(<div className="light square" />);
+        } else if (squares.length <= firstPlayerPiece) {
+          squares.push(
+            <div className="dark square">
+              <div className="red chip" />
+            </div>
+          );
+        } else if (squares.length >= secondPlayerPiece) {
+          squares.push(
+            <div className="dark square">
+              <div className="black chip" />
+            </div>
+          );
+        } else {
+          squares.push(<div className="dark square" />);
+        }
       }
     }
-  }
+  };
+
+  checkerContainer();
 
   return (
     <div className="checker-board">
