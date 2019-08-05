4f537cc0522cc35a3cbe5d40cd74862fc5b817bc
Cryosith <caseyvandyke@ymail.com>
2019-08-04 20:01:07 -0600

    checker squares

--- src/CheckerSquare.jsx
+++ src/CheckerSquare.jsx
@@ -7,8 +7,6 @@ const CheckerSquare = props => {
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < row; j++) {
       if ((i + j) % 2 === 0) {
-        const firstPlayer = squares.slice(1, 24);
-
         squares.push(<div className="light square" />);
       } else {
         squares.push(<div className="dark square" />);
